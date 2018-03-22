d93b2d38c62d811d1c5a0c19cd2841875d2bbabb
Yen Chen Wee <yenchenwee@wireless-10-145-125-19.public.utexas.edu>
2018-03-21 20:09:33 -0500

    Added screens for group 3

---
+++ PrepMate/AddRecipeSecondPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddRecipeSecondPageViewController.swift
+//  PrepMate
+//
+//  Created by Yen Chen Wee on 3/21/18.
+//  Copyright © 2018 Joseph Bourque. All rights reserved.
+//
+
+import UIKit
+
+class AddRecipeSecondPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
