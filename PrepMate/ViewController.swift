efa4c31d7dd79dabebbd93352c764e7cc67d74ad
Joseph Bourque <indegon1@gmail.com>
2018-03-22 19:38:32 -0500

    Got splash screen animation working

--- PrepMate/ViewController.swift
+++ PrepMate/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadIcon.animationImages = [#imageLiteral(resourceName: "frame_00_delay-0.04s.png"), #imageLiteral(resourceName: "frame_01_delay-0.04s.png"), #imageLiteral(resourceName: "frame_02_delay-0.04s.png"), #imageLiteral(resourceName: "frame_03_delay-0.04s.png"), #imageLiteral(resourceName: "frame_04_delay-0.04s.png"), #imageLiteral(resourceName: "frame_05_delay-0.04s.png"), #imageLiteral(resourceName: "frame_06_delay-0.04s.png"), #imageLiteral(resourceName: "frame_07_delay-0.04s.png"), #imageLiteral(resourceName: "frame_08_delay-0.04s.png"), #imageLiteral(resourceName: "frame_09_delay-0.04s.png"),
+                                    #imageLiteral(resourceName: "frame_10_delay-0.04s.png"), #imageLiteral(resourceName: "frame_11_delay-0.04s.png"), #imageLiteral(resourceName: "frame_12_delay-0.04s.png"), #imageLiteral(resourceName: "frame_13_delay-0.04s.png"), #imageLiteral(resourceName: "frame_14_delay-0.04s.png"), #imageLiteral(resourceName: "frame_15_delay-0.04s.png"), #imageLiteral(resourceName: "frame_16_delay-0.04s.png"), #imageLiteral(resourceName: "frame_17_delay-0.04s.png"), #imageLiteral(resourceName: "frame_18_delay-0.04s.png"), #imageLiteral(resourceName: "frame_19_delay-0.04s.png"),
+                                    #imageLiteral(resourceName: "frame_20_delay-0.04s.png"), #imageLiteral(resourceName: "frame_21_delay-0.04s.png"), #imageLiteral(resourceName: "frame_22_delay-0.04s.png"), #imageLiteral(resourceName: "frame_23_delay-0.04s.png")]
+        loadIcon.animationDuration = 1.0
+        loadIcon.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
