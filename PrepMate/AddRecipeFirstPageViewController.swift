61a2a74a84f38c29460fd8a343a82a204db184d0
Yen Chen Wee <yenchenwee@wireless-10-147-109-100.public.utexas.edu>
2018-04-04 12:04:18 -0500

    WIP: Beta work. Pushing Associated types file

--- PrepMate/AddRecipeFirstPageViewController.swift
+++ PrepMate/AddRecipeFirstPageViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class AddRecipeFirstPageViewController: UIViewController {
+class DirectionTableCell: UITableViewCell {
+    @IBOutlet weak var directionLabel: UILabel!
+}
+
+class IngredientTableCell: UITableViewCell {
+    @IBOutlet weak var ingLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+}
+class AddRecipeFirstPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var recipeNameField: UITextField!
     @IBOutlet weak var categoryPicker: UIPickerView!
     @IBOutlet weak var servingsPicker: UIPickerView!
     @IBOutlet weak var prepTimeField: UITextField!
     @IBOutlet weak var cookTimeField: UITextField!
+    @IBOutlet weak var directionTableview: UITableView!
+    @IBOutlet weak var ingTableView: UITableView!
+    var directionList = [String]()
+    var ingList = [Ingredient]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == self.directionTableview {
+            return directionList.count
+        }
+        if tableView == self.ingTableView {
+            return ingList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == self.directionTableview {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "directionTableCell", for: indexPath as IndexPath) as! DirectionTableCell
+        }
+        if tableView == self.ingTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientTableCell", for: indexPath as IndexPath) as! IngredientTableCell
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
--- PrepMate/RecipeNutritionViewController.swift
+++ PrepMate/RecipeNutritionViewController.swift
@@ -8,20 +8,58 @@
 
 import UIKit
 
-class RecipeNutritionViewController: UIViewController {
-    // TODO: Create outlet for collection view that makes up the nutrition overview
+/// Class for nutrition collection view cell
+class nutritionOverviewCell: UICollectionViewCell {
+    // Image contained within each cell
+    @IBOutlet weak var nutritionImage: UIImageView!
+}
+
+class RecipeNutritionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+
     var recipe = Recipe()
+    var nutritionList = [UIImage]()
+    @IBOutlet weak var nutritionCollectionView: UICollectionView!
     @IBOutlet weak var basicInfoText: UITextView!
     @IBOutlet weak var vitaminInfoText: UITextView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.nutritionList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nutritionOverviewCell", for: indexPath as IndexPath) as! nutritionOverviewCell
+        let row = indexPath.row
+        cell.nutritionImage.image = self.nutritionList[row]
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.basicInfoText.layer.borderWidth = 1
+        self.vitaminInfoText.layer.borderWidth = 1
+        for i in 0...(nutritionImages.count-1){
+            if self.recipe.getFlags() & 1 << i == 1 {
+                self.nutritionList.append(nutritionImages[i])
+            }
+        }
+        self.basicInfoText.text! = self.loadBasicInfo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func loadBasicInfo() -> String {
+        let maxWidth = 20
+        var headerString = "Amount"
+        var result = headerString.padding(toLength: maxWidth, withPad: " ", startingAt: 0)
+        result = result + "Daily Value %\n"
+        
+        
+
+        
+        return result
+        
     }
     
 
