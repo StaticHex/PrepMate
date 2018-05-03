3314fdba2d8f787f298fc7b8fca87c6084266a86
Pablo Velasco <pulvel@gmail.com>
2018-05-02 19:17:30 -0500

    Correctly showing bookmarks and favorites recipe selection

--- PrepMate/AddMealsViewController.swift
+++ PrepMate/AddMealsViewController.swift
@@ -66,7 +66,11 @@ class AddMealsViewController: UIViewController, UITableViewDelegate, UITableView
     var mealsToChooseFrom = [MealOptions]()
     var meal = Meal()
     
-    var recipeList = getRecipes(query: "id>=1 ORDER BY rating ASC LIMIT 10")
+    var eMsg = ""
+    var userRecipeList = [UserRecipe]() // Used for the recipe box and favorites list
+    
+    var recipeListBookmarks = [Recipe]()
+    var recipeListFavorites = [Recipe]()
     
     var existingMeal = false
     
@@ -87,7 +91,15 @@ class AddMealsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        self.mealsToChooseFrom = [MealOptions(name: "Recipe Box", items: self.recipeList), MealOptions(name: "Favorites", items: self.recipeList)]
+        self.getUserRecipes(uid: currentUser.getId())
+        let tupleStrFavorites = ((0..<self.userRecipeList.count).filter({self.userRecipeList[$0].favorite == 1}).map{"\(self.userRecipeList[$0].recipeId!)"}).joined(separator: ", ")
+        let tupleStrBookmarks = ((0..<self.userRecipeList.count).filter({self.userRecipeList[$0].favorite == 0}).map{"\(self.userRecipeList[$0].recipeId!)"}).joined(separator: ", ")
+        var recipesFavorites = getRecipes(query: "id IN (\(tupleStrFavorites))")
+        var recipeBookmarks = getRecipes(query: "id IN (\(tupleStrBookmarks))")
+        
+        self.recipeListBookmarks = recipeBookmarks
+        self.recipeListFavorites = recipesFavorites
+        self.mealsToChooseFrom = [MealOptions(name: "Recipe Box", items: self.recipeListBookmarks), MealOptions(name: "Favorites", items: self.recipeListFavorites)]
         mealsChosenRecipeBox = [Bool](repeating: false, count: mealsToChooseFrom[0].items.count)
         mealsChosenFavorites = [Bool](repeating: false, count: mealsToChooseFrom[1].items.count)
     }
@@ -181,5 +193,106 @@ class AddMealsViewController: UIViewController, UITableViewDelegate, UITableView
         self.present(alert, animated: true, completion: nil)
     }
 
+    
+    func getUserRecipes(uid:Int) -> Bool {
+        // reset the list
+        self.userRecipeList.removeAll()
+        
+        // create a variable to return whether we errored out or not
+        var vError : Bool = false
+        
+        // path to our backend script
+        let URL_VERIFY = "http://www.teragentech.net/prepmate/GetUserRecipes.php"
+        
+        // variable which will spin until verification is finished
+        var finished : Bool = false
+        
+        // create our URL object
+        let url = URL(string: URL_VERIFY)
+        
+        // create the request and set the type to POST, otherwise we get authorization error
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        let params = "id=\(uid)"
+        
+        request.httpBody = params.data(using: String.Encoding.utf8)
+        
+        // Create a task and send our request to our REST API
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            // if we error out, return the error message
+            if(error != nil) {
+                self.eMsg = error!.localizedDescription
+                finished = true
+                vError = true
+                return
+            }
+            
+            // If there was no error, parse the response
+            do {
+                // convert response to a dictionary
+                let JSONResponse = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                // Get the error status and the error message from the database
+                if let parseJSON = JSONResponse {
+                    vError = (parseJSON["error"] as! Bool)
+                    self.eMsg = parseJSON["msg"] as! String
+                    if(!vError) {
+                        var count = 0
+                        while let record = parseJSON["\(count)"] as? String {
+                            if let row = try JSONSerialization.jsonObject(with: record.data(using: String.Encoding.utf8)!, options: .mutableContainers) as? NSDictionary {
+                                var current = UserRecipe()
+                                if let urId = row["id"] as? String {
+                                    current.id = Int(urId)!
+                                    if let urRid = row["recipe_id"] as? String {
+                                        current.recipeId = Int(urRid)!
+                                        if let urRName = row["recipe_name"] as? String {
+                                            current.recipeName = urRName
+                                            if let urRating = row["recipe_rating"] as? String {
+                                                current.recipeRating = Int(urRating)!
+                                                if let urFav = row["favorite"] as? String {
+                                                    current.favorite = Int(urFav)!
+                                                    self.userRecipeList.append(current)
+                                                } else {
+                                                    vError = true
+                                                    self.eMsg = "Error occured while parsing user recipe favorite in get user recipes"
+                                                    return
+                                                }
+                                            } else {
+                                                vError = true
+                                                self.eMsg = "Error occured while parsing recipe rating in get user recipes"
+                                                return
+                                            }
+                                        } else {
+                                            vError = true
+                                            self.eMsg = "Error occured while parsing recipe name in get user recipes"
+                                            return
+                                        }
+                                    } else {
+                                        vError = true
+                                        self.eMsg = "Error occured while parsing recipe id in get user recipes"
+                                        return
+                                    }
+                                } else {
+                                    vError = true
+                                    self.eMsg = "Error occured while parsing user recipe id in get user recipes"
+                                    return
+                                }
+                            }
+                            count += 1
+                        }
+                    }
+                }
+            } catch {
+                self.eMsg = error.localizedDescription
+                vError = true
+            }
+            finished = true
+        }
+        // execute our task and then return the results
+        task.resume()
+        while(!finished) {}
+        return vError
+    }
 
 }
